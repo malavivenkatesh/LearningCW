090e88111bdced9b29a776ba92661efbd3f7bbdd
Malavika Venkatesh <s1709617@fuente.inf.ed.ac.uk>
2019-03-28 01:01:25 +0000

    done task 1

--- task1_1.m
+++ task1_1.m
@@ -3,6 +3,23 @@
 function task1_1(X, Y)
 % Input:
 %  X : M-by-D data matrix (double)
-%  Y : M-by-1 label vector (unit8)
+%  Y : M-by-1 label vector (unit8) 
+
+[rw cw] = size(X);
+result = zeros(28, 28, 10);
+for label=0:9
+    count = 1;
+    k = 1;
+    while count < 11
+    if Y(k) == label        
+        img =  reshape(X(k,:), 28, 28)';
+        result(:, :, count) = img;
+
+        count = count + 1;
+    end
+    k = k + 1;
+    end
+    montage(result);
+end
 
 end
