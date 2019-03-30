ccacfb35cbe9be0474a3d0bd8b8d84fe47eb0965
Malavika Venkatesh <s1709617@pillay.inf.ed.ac.uk>
2019-03-30 23:36:55 +0000

    finish to 1.2)

--- task1_1.m
+++ task1_1.m
@@ -6,13 +6,13 @@ function task1_1(X, Y)
 %  Y : M-by-1 label vector (unit8) 
 
 [rw cw] = size(X);
-result = zeros(28, 28, 10);
+result = zeros(28, 28, 1, 10);
 for label=0:9
     count = 1;
     k = 1;
     while count < 11
     if Y(k) == label        
-        result(:, :, count) = reshape(X(k,:), 28, 28)';
+        result(:, :,1, count) = reshape(X(k,:), 28, 28)';
 
         count = count + 1;
     end
--- task1_2.m
+++ task1_2.m
@@ -8,5 +8,34 @@ function M = task1_2(X, Y)
 %  M : (K+1)-by-D mean vector matrix (double)
 %      Note that M(K+1,:) is the mean vector of X.
 
+[rw, cl] = size(X);
+sums = zeros(11, 784);
+counts = zeros(11, 1);
+mean = zeros(11, 784);
+result = zeros(28, 28, 1, 11);
+
+for label=0:9
+    count = 0;
+    for i=1:rw
+        if Y(i) == label
+            count = count + 1;
+            sums((label + 1), :) = (sums((label+1), :) + X(i, :));
+            counts((label + 1), 1) = count;
+            result(:, :, 1, (label + 1)) = result(:, :, 1, (label + 1)) + reshape(X(i, :), 28, 28, 1)';
+        end
+    end
+end
+
+for i=1:10
+    mean(i,:) = (sums(i, :) ./ counts(i, 1));
+    result(:, :, 1, i) = result(:, :, 1, i) ./ counts(i, 1);
+end
+
+mean(11, :) = sum(X) ./ rw;
+result(:, :, 1, 11) = reshape(mean(11, :), 28, 28, 1)';
+
+M = mean;
+
+montage(result);
 
 end
