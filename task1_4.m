735b186424b14bf1af954ebfdbe2bde045ae5374
Malavika Venkatesh <s1709617@pillay.inf.ed.ac.uk>
2019-03-31 02:06:38 +0100

    finish 1.4

--- task1_4.m
+++ task1_4.m
@@ -4,6 +4,16 @@ function task1_4(EVecs)
 % Input:
 %  Evecs : the same format as in comp_pca.m
 %
+EVecs = EVecs';
 
+[rw cw] = size(EVecs);
+result = zeros(28, 28, 1, 12);
+for i=1:10  
+   result(:, :,1, i) = reshape(EVecs(i,:)*255.0, 28, 28)';
+end
+
+result = mat2gray(result);
+montage(result);
 
 end
+
