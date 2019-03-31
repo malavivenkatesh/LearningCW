0b60532778b7e44419dd06902b1d6ae380d593f3
Malavika Venkatesh <s1709617@pillay.inf.ed.ac.uk>
2019-03-31 00:57:58 +0000

    finish to 1.3

--- comp_pca.m
+++ comp_pca.m
@@ -8,9 +8,39 @@ function [EVecs, EVals] = comp_pca(X)
 %       Eigenvalues in descending order, D x 1 vector (double)
 %   (Note that the i-th columns of Evecs should corresponds to the i-th element in EVals)
   %% TO-DO
+  
+    %Calculating the mean mean
+    x_mean = My_Mean(X);
     
-    EVecs = nan;
-    EVals = nan;
+    %Subtracting the mean from every value in X
+    X = bsxfun(@minus, X, x_mean);
+    
+    %calculating the covariance
+    N = length(X);
+    covar_m = 1/(N-1)*(X' * X);
+    
+    %using eig with a now square matrix.
+    [PC, V] = eig(covar_m);
+    
+    %Diagonalising the eigenvalues and sorting the vectors in descending
+    %order
+    V = diag(V);
+    [tmp, ridx] = sort(V, 1, 'descend');
+    
+    %Sorting the matrices in descending order using the index from the sort
+    %function
+    V = V(ridx);
+    PC = PC(:,ridx);
+    
+    [pc_rows, pc_cols] = size(PC);
+    for i=1:pc_cols
+        if PC(1,i) < 0
+            PC(:, i) = - PC(:, i);
+        end
+    end
+    
+    EVecs = PC;
+    EVals = V;
 
 end
 
--- task1_3.m
+++ task1_3.m
@@ -9,4 +9,25 @@ function [EVecs, EVals, CumVar, MinDims] = task1_3(X)
 %  MinDims : 4-by-1 vector (integer) of the minimum number of PCA dimensions
 %            to cover 70%, 80%, 90%, and 95% of the total variance.
 
+[EVecs, EVals] = comp_pca(X);
+CumVar = cumsum(EVals);
+
+[evals_rows, evals_cols] = size(EVals);
+
+MinDims = zeros(4,1);
+
+for i=1:evals_rows
+    
+    curr_cumvar_ratio = CumVar(i) / CumVar(evals_rows);
+    
+    if (curr_cumvar_ratio < 0.8) && (curr_cumvar_ratio >=0.7)
+        MinDims(1,1) = i;
+    elseif (curr_cumvar_ratio < 0.9) && (curr_cumvar_ratio >=0.8)
+        MinDims(2,1) = i;
+    elseif (curr_cumvar_ratio < 0.95) && (curr_cumvar_ratio >=0.9)
+        MinDims(3,1) = i;
+    elseif (curr_cumvar_ratio >=0.95)
+        MinDims(4,1) = i;
+    end
+
 end
