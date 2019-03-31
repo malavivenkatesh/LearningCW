4e19202078c77616a89f138bc4b76c65cd4e3847
Malavika Venkatesh <s1709617@vonn.inf.ed.ac.uk>
2019-03-31 23:52:40 +0100

    finish to 1.7

--- task1_7.m
+++ task1_7.m
@@ -12,6 +12,58 @@ function Dmap = task1_7(MAT_ClusterCentres, MAT_M, MAT_evecs, MAT_evals, posVec,
 % Output
 %  Dmap  : nbins-by-nbins matrix (uint8) - each element represents
 %	   the cluster number that the point belongs to.
+load(MAT_ClusterCentres);
+[K, cl] = size(C);
 
+load(MAT_M);
+meanvector = M(11, :);
+
+load(MAT_evecs);
+load(MAT_evals);
+
+%standard deviations of x and y axis
+xstan = sqrt(EVals(1));
+ystan = sqrt(EVals(2));
+
+pca1 = EVecs(:, 1);
+pca2 = EVecs(:, 2);
+pcas = [pca1, pca2];
+m_proj = (C - posVec)*pcas;
+mean_proj = meanvector*pcas
+
+
+xrange = meanvector(1);
+yrange = meanvector(2);
+
+
+Xplot = linspace(xrange - 5*xstan, xrange + 5*xstan, nbins)';
+Yplot = linspace(yrange - 5*ystan, yrange + 5*ystan, nbins)';
+[Xv, Yv] = meshgrid(Xplot, Yplot);
+gridX = [Xv(:), Yv(:)]; 
+
+%X_back_proj = (V*(gridX(1))' + posVec')';
+%Y_back_proj = (V*(gridX(2))' + posVec')';
+
+Dmap = length(Xv(:));
+for i = 1:length(gridX)  
+dists = MySqDist(m_proj, gridX(i, :))'; % Compute distances
+[d I] = sort(dists, 'ascend');
+Dmap(i) = I(1);
+end
+
+figure;
+
+[CC,h] = contourf(Xplot(:), Yplot(:), reshape(Dmap, length(Xplot), length(Yplot)));
+set(h,'LineColor','none');
+%colormap(cmap); 
+hold on;
+
+% Plot the scatter plots grouped by their classes
+%scatters = gscatter(A(:,1), A(:,2), C, [0,0,0], 'o', 4);
+% Fill in the color of each point according to the class labels.
+
+%for n = 1:length(scatters)
+%set(scatters(n), 'MarkerFaceColor', cmap(n,:));
+%end
 			  
 end
